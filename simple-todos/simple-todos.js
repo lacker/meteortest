3d3aa929d95e0dbc17286d0dac13aab89dc8ea56
Kevin Lacker <lacker@gmail.com>
2015-10-10 14:06:12 -0700

    db migration in meteor

--- simple-todos/simple-todos.js
+++ simple-todos/simple-todos.js
@@ -1,28 +1,25 @@
-if (Meteor.isClient) {
-  // This code only runs on the client
-  Template.body.helpers({
-    tasks: [
-      { text: "This is task 1" },
-      { text: "This is task 2" },
-      { text: "This is task 3" }
-    ]
-  });
-}
-
 Tasks = new Mongo.Collection("tasks");
 
-Tasks.insert({
-  text: "yolo " + Math.floor(1000 * Math.random()),
-  createdAt: new Date(),
-});
 
 if (Meteor.isClient) {
-  // This code only runs on the client
+  Tasks.insert({
+    text: "yolo " + Math.floor(1000 * Math.random()),
+    createdAt: new Date(),
+  });
+
   Template.body.helpers({
     tasks: function() {
-      return Tasks.find({createdAt: {'$exists': true}},
-                        {sort: {createdAt: -1}});
+      return Tasks.find({}, {sort: {createdAt: -1}});
     }
   });
 }
 
+if (Meteor.isServer) {
+  // Delete the messages with no date
+  let old = Tasks.find({createdAt: {'$exists': false}}).fetch();
+  for (var object of old) {
+    Tasks.remove(object._id);
+  }
+
+  console.log('the server is running');
+}
